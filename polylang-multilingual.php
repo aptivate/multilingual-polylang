89420a270de952e3578078195bcdaff6ae4958b3
Martin Burchell <martinb@aptivate.org>
2016-10-20 14:43:08 +0100

    Add plugin to show posts in both languages

---
+++ polylang-multilingual.php
@@ -0,0 +1,70 @@
+<?php
+/*
+  Plugin Name: Polylang Multilingual
+  Description: Display posts in all languages
+  Version: 1.0.0
+  Author: Aptivate
+*/
+
+class PolylangMultilingual {
+	private static $current_language;
+	private static $other_languages;
+
+	public static function init() {
+		if ( self::is_polylang_installed() && ! is_admin() ) {
+			self::$current_language = pll_current_language();
+			self::$other_languages = array_diff(
+				pll_languages_list(),
+				array( self::$current_language )
+			);
+
+			add_filter(
+				'pre_get_posts',
+				array( 'PolylangMultilingual', 'add_post_filters' )
+			);
+		}
+	}
+
+	private static function is_polylang_installed() {
+		return function_exists( 'pll_languages_list' );
+	}
+
+	public static function add_post_filters( $query ) {
+		/* https://wordpress.org/support/topic/show-posts-from-other-languages/?replies=18
+		 * by way of
+		 * http://wordpress.syllogic.in/2014/08/going-multi-lingual-with-polylang/
+		 */
+		if ( $query->is_main_query() && is_home() ) {
+			$duplicated_post_ids = self::get_duplicated_posts();
+
+			$all_languages = '';
+			$query->set( 'lang' , $all_languages );
+			$query->set( 'post__not_in', $duplicated_post_ids );
+		}
+
+		return $query;
+	}
+
+	private static function get_duplicated_posts() {
+		$duplicate_ids = array();
+
+		$terms = get_terms( 'post_translations' );
+
+		foreach ( $terms as $translation ) {
+			$trans_post = unserialize( $translation->description );
+			if ( self::is_in_current_language( $trans_post ) ) {
+				foreach ( self::$other_languages as $other_language ) {
+					$duplicate_ids[] = $trans_post[ $other_language ];
+				}
+			}
+		}
+
+		return $duplicate_ids;
+	}
+
+	private static function is_in_current_language( $trans_post ) {
+		return $trans_post[ self::$current_language ] != 0;
+	}
+}
+
+add_action( 'plugins_loaded', array( 'PolylangMultilingual', 'init' ) );
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Polylang Multilingual ===
+Contributors: Aptivate
+Tags: polylang, multilingual, bilingual, translate, translation, language, multilanguage, international, localization
+Requires at least: 4.0
+Tested up to: 4.6.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+This plugin extends [polylang](https://wordpress.org/plugins/polylang/) to
+modify post queries, such that posts that have no translation in the currently
+selected language appear in results in their own language.
+
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory.
+2. Activate it through the **Plugins** menu in WordPress.
+
+
+== Changelog ==
+
+= 1.0.0 =
+* First version
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* First version
