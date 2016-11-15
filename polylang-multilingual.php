d1603ce0cd15ad96656bd14ddc8a9a2e9af06dbe
Martin Burchell <martinb@aptivate.org>
2016-11-15 12:20:22 +0000

    Move disabling of URL redirection into plugin

--- polylang-multilingual.php
+++ polylang-multilingual.php
@@ -17,6 +17,11 @@ class PolylangMultilingual {
 				pll_languages_list(),
 				array( self::$current_language )
 			);
+
+			/* Don't redirect URLs in 'wrong' language so that we can
+			 * have article in one language and interface in another
+			 */
+			add_filter( 'pll_check_canonical_url', '__return_false' );
 		}
 	}
 
